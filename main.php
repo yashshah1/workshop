c9ac2ed363cdc7004d8009e3315cc8998c444efb
Yash Shah <yashah1234@gmail.com>
2019-10-28 18:23:54 +0530

    commit

--- main.php
+++ main.php
@@ -4,7 +4,27 @@
     $pass = "password";
     $dbname = "workshop";
 
-    if (isset($_POST['fetch_all'])) {
-        
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $headers = getallheaders();
+    if ($headers["Content-Type"] == "application/json") {
+        $_POST = json_decode(file_get_contents("php://input"), true);
+    }
+    if (isset($_POST['fetchall'])) {
+        $query = "select * from tasks";
+        $result = $conn->query($query);
+        $rows = array();
+
+
+        while($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        $response = array();
+        $response['length'] = count($rows);
+        $response['data'] = $rows;
+        echo json_encode($response);
     }
 ?>
