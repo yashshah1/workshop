a780182e3bc57ac202483d3f44ea1812fd7a2654
Yash Shah <yashah1234@gmail.com>
2019-10-29 14:06:06 +0530

    lets see

--- main.php
+++ main.php
@@ -27,4 +27,52 @@
         $response['data'] = $rows;
         echo json_encode($response);
     }
+    if(isset($_POST)) {
+        foreach($_POST as $key => $value){
+            if($key[0] == 'D') {
+                $id = substr($key, 1);
+                $query = "delete from tasks where id=$id";
+                $alert_result = $conn->query($query);
+                if($alert_result) {
+                    $message = 'Deleted successfully';
+                }
+            }
+            else if ($key[0] == 'C') {
+                // $id = substr($key, 1);
+                $id = $_POST['id'];
+                $query = "update tasks set status = 1 where id=$id";
+                $alert_result = $conn->query($query);
+                if($alert_result) {
+                    $res = array();
+                    $res['status'] = 200;
+                    echo json_encode($res);
+                }  
+            }
+            else if ($key[0] == 'U') {
+                $id = substr($key, 1);
+                $newname = $_POST['newname'];
+
+                $query = "update tasks set task=\"$newname\" where id=$id";
+                $alert_result = $conn->query($query);
+                if($alert_result) {
+                    $message = 'Updated task';
+                } 
+            }
+        }
+    }
+    // if (isset($_POST['complete'])) {
+    //     echo json_encode("")
+    //     $query = "select * from tasks";
+    //     $result = $conn->query($query);
+    //     $rows = array();
+
+
+    //     while($row = $result->fetch_assoc()) {
+    //         $rows[] = $row;
+    //     }
+    //     $response = array();
+    //     $response['length'] = count($rows);
+    //     $response['data'] = $rows;
+    //     echo json_encode($response);
+    // }
 ?>
