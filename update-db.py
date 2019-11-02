624fcd49841bfa5ceb0290771cbaffe0cb547fc5
Yash Shah <yashah1234@gmail.com>
2019-11-02 12:59:27 +0530

    updated password

--- update-db.py
+++ update-db.py
@@ -1,20 +1,8 @@
-import pymysql
 import sys
 try:
 	n = int(sys.argv[1])
 except:
 	n = 10
-# Open database connection
-db = pymysql.connect("localhost","yash","password","workshop" )
-
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-
 q = "INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (NULL, 'test-task-{}', '0');"
 for i in range(n):
 	print(q.format(i + 1))
-	cursor.execute(q.format(i + 1))
-
-
-# disconnect from server
-db.close()
