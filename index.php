ff159ef743b4a51ef5661e1de838e566303b841e
Yash Shah <Yash.Shah@wkfs-frc.local>
2019-11-03 20:10:47 +0530

    Finished part-i

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
-    $user = "yash";
-    $pass = "password";
+    $user = "root";
+    $pass = "";
     $dbname = "workshop";
 
     $conn = new mysqli($host, $user, $pass, $dbname);
