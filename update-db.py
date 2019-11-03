805909b95560e39b7c19d0199a441f5e351dfc8d
Yash Shah <Yash.Shah@wkfs-frc.local>
2019-11-03 20:15:45 +0530

    removed python script

--- update-db.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-	n = int(sys.argv[1])
-except:
-	n = 10
-q = "INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (NULL, 'test-task-{}', '0');"
-for i in range(n):
-	print(q.format(i + 1))
